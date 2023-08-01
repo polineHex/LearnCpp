d8a5872b5c99c09fbb24f4530732ef0eca642b6f
Poline <nomail@nonomail.com>
2023-08-01 13:19:15 +0200

    Added a question on architecture

--- src/Physics/Components/VelocityComponent.h
+++ src/Physics/Components/VelocityComponent.h
@@ -8,6 +8,9 @@ namespace game
 struct VelocityComponent {
 	Vector2 mDirection{};
 	Vector2 mPrevDirection{};
+	
+	//QUESTION: not happy that this is in velocity, but new position is possible only if there is velocity, so i dont want to drag it around
+	//in transformComponent, but should i create an extra component just to store this?
 	Vector2 mNewPossiblePosition{};
 	float mSpeed{};
 
